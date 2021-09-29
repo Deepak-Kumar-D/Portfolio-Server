4dad501cb2b2d5d7ba9f486dd3b1df4086bfc021
Deepak Kumar D <deepak.kumard36@gmail.com>
2021-09-29 18:11:15 +0530

    cors options removed

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || process.env.LOCAL_PORT;
+const PORT = process.env.PORT || 5000;
 
-const corsOptions = { origin: true, credentials: true };
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
